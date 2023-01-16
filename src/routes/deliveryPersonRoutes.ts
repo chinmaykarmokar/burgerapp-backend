b99dd967f32325ee6b8c29bb2c0bdecf4d89080d
Chinmay Karmokar <chinmaykarmokar@gmail.com>
2023-01-17 01:45:32 +0530

    Changes in the some of the admin API endpoints and Delivery Person API endpoints. Some refactoring.

--- src/routes/deliveryPersonRoutes.ts
+++ src/routes/deliveryPersonRoutes.ts
@@ -7,6 +7,7 @@ import { Orders } from "../Entities/OrdersEntity";
 
 // Import authentication middleware
 import authenticateDeliveryPersonToken from "../../middlewares/deliveryPersonAuthMiddleware";
+import { Not } from "typeorm";
 
 // Parse JSON and use Router
 const app = express();
@@ -44,7 +45,7 @@ router.post("/deliveryPersonRegister", async (req: Request, res: Response) => {
         else {
             await connectDB.getRepository(DeliveryPerson).insert(deliveryPersonPayload);
             res.json({
-                message: "Delivery Person successfully resgitered on our platform."
+                message: "Delivery Person successfully registered on our platform."
             })
         }
     }
@@ -66,6 +67,24 @@ router.get("/deliveryPerson", authenticateDeliveryPersonToken, async (req: any, 
     })
 })
 
+// Get order assigned to delivery person
+router.get("/orderAssigned", authenticateDeliveryPersonToken, async (req: any, res: any) => {
+    const findOrdersAssigned = await connectDB.getRepository(DeliveryPerson).find({
+        where: [{email: req.user.email, order_id : Not(0)}]
+    })
+
+    if (!findOrdersAssigned) {
+        res.json({
+            message: "No orders assigned."
+        })
+    }
+    else {
+        res.json({
+            data: findOrdersAssigned
+        })
+    }
+})
+
 // Update order status on completion
 router.post("/orderCompletion", authenticateDeliveryPersonToken, async (req: any, res: any) => {
     const findDeliveryPerson = await connectDB.getRepository(DeliveryPerson).findOne({
@@ -76,7 +95,21 @@ router.post("/orderCompletion", authenticateDeliveryPersonToken, async (req: any
         where: {id: findDeliveryPerson?.order_id}
     })
 
-    await connectDB.getRepository(DeliveryPerson).merge(findDeliveryPerson!, {status: "available"});
+    // const orderUpdateData = {
+    //     status: "available",
+    //     items_to_be_delivered: " ",
+    //     delivery_address: " ",
+    //     order_id: 0
+    // }
+
+    const orderUpdateData = {
+        status: req.body.status,
+        items_to_be_delivered: req.body.items_to_be_delivered,
+        delivery_address: req.body.delivery_address,
+        order_id: req.body.order_id
+    }
+
+    await connectDB.getRepository(DeliveryPerson).merge(findDeliveryPerson!, orderUpdateData);
     await connectDB.getRepository(DeliveryPerson).save(findDeliveryPerson!);
 
     await connectDB.getRepository(Orders).merge(findOrderToUpdate!,{delivery_status: "Completed"});
