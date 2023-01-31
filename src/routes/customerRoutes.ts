a26d7bb51efcc21824b3023863afec7eeda6a794
Chinmay Karmokar <chinmaykarmokar@gmail.com>
2023-01-31 21:40:06 +0530

    Minor change in customer routes.

--- src/routes/customerRoutes.ts
+++ src/routes/customerRoutes.ts
@@ -407,22 +407,22 @@ router.post("/createOrder", authenticateCustomerToken, async (req: any, res: any
                 })
             }
 
-            let message = {
-                to: "chinmaykarmokar@gmail.com",
-                from: "burpger.dine@gmail.com",
-                subject: "Inventory running low",
-                html: `
-                <p>
-                    Hello <b>Chinmay</b>, this is to inform you that certain items in the inventory are running low on stock. You may have a look at them. 
-                    <br/>
-                    Thank you!
-                </p>`
-            }
+            // let message = {
+            //     to: "chinmaykarmokar@gmail.com",
+            //     from: "burpger.dine@gmail.com",
+            //     subject: "Inventory running low",
+            //     html: `
+            //     <p>
+            //         Hello <b>Chinmay</b>, this is to inform you that certain items in the inventory are running low on stock. You may have a look at them. 
+            //         <br/>
+            //         Thank you!
+            //     </p>`
+            // }
 
-            sgMail.send(message)
-            .then((response: any) => {
-                console.log(`Email has been sent to customer ${req.body.email}.`)
-            })
+            // sgMail.send(message)
+            // .then((response: any) => {
+            //     console.log(`Email has been sent to customer ${req.body.email}.`)
+            // })
     
             const updateTomatoesInTheInventory = {
                 quantity: getTomatoesFromInventory!.quantity - totalTomatoes
